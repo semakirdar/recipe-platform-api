2b7efbb7f1d6a324245b7b440fc237b5efb7548f
Sema Kırdar <sema@dijitaladam.com>
2021-09-17 17:05:20 +0300

    Profile update edit.

---
+++ app/Http/Controllers/Auths/ProfileController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\Auths;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Profile\UpdateRequest;
+use App\Models\Profile;
+use Illuminate\Http\Request;
+
+class ProfileController extends Controller
+{
+    public function update(UpdateRequest $request)
+    {
+        $profile = Profile::query()->where('user_id', auth()->user()->id)->first();
+
+        $profile->update([
+            'bio' => $request->bio,
+            'website' => $request->website,
+            'location' => $request->location
+        ]);
+
+        return response()->json([
+            'success' => true,
+            'error' => 'Your profile updated'
+        ]);
+    }
+}
--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -17,5 +17,4 @@ class Profile extends Model
     ];
 
 
-
 }
