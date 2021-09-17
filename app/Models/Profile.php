d65041b935b4f71d9227c71715d7254cad3336fa
Sema Kırdar <sema@dijitaladam.com>
2021-09-17 14:14:39 +0300

    model dosyaları oluşturuldu.

---
+++ app/Models/Profile.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Profile extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_İd',
+        'location',
+        'website',
+        'bio'
+    ];
+
+
+
+}
